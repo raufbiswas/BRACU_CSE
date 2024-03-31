f7199038747a4b99d10cdffb50916582b11329ae
B M Rauf <bmrauf@outlook.com>
2024-03-31 23:11:51 +0600

    Update

--- CSE220/Practice Sheet/LAB Final Fall 23/Solution.py
+++ CSE220/Practice Sheet/LAB Final Fall 23/Solution.py
@@ -1,15 +1,26 @@
 def print_pattern(n):
-    if n >= 47:
+    funCall(n, spaces = 0)
+
+def funCall(n, spaces):
+    if n >= 40:
+        return
+    print(" " * spaces, end="")
+    pattern(n)
+    print()
+    funCall(n + 7, spaces + len(str(n)) + 1)
+
+def pattern(n):
+    if n >= 40:
+        print(n, end=" ")
         return
     print(n, end=" ")
-    print_pattern(n + 7)
-    if n < 40:
-        print(n, end=" ")
+    pattern(n + 7)
+    print(n, end=" ")
 
-def call(n):
-    if n < 40:
-        print_pattern(n)
-        print()
-        call(n+7)
-n = int(input())
-call(n)
+print('Sample Output 1')
+n = 18
+print_pattern(n)
+
+print('Sample Output 2')
+n = 12
+print_pattern(n)
